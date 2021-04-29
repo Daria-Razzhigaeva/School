670a5aaf8a81e2cd65761c71d27832ddf92023e9
Daria Razzhigaeva <dariarazzhigaeva@Darias-MacBook-Pro.local>
2021-04-29 15:12:30 -0700

    First commit

---
+++ src/school/Parent.java
@@ -0,0 +1,29 @@
+package school;
+
+public final class Parent extends BasePerson {
+    private long phoneNumber;
+    private Student student;
+
+    Parent(String firstName, String lastName, char gender, int age, String status, long phoneNumber, Student student) {
+        super(firstName, lastName, gender, age, status);
+        this.phoneNumber = phoneNumber;
+        this.student = student;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void printParent(){
+        System.out.println();
+        System.out.println(
+                getFirstName() + " " + getLastName() + ", is parent of school.Student " + student.getFirstName() +
+                        student.getLastName() + ", grade " + student.getGrade() + "."
+        );
+        System.out.println("school.Parent's phome number: " + getPhoneNumber());
+    }
+}
