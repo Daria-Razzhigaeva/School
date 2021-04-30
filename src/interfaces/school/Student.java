e16ef91fa5db1987d267273450169aef4745a829
Daria Razzhigaeva <dariarazzhigaeva@Darias-MacBook-Pro.local>
2021-04-29 23:07:21 -0700

    new package interfaces created, code fron package school copied to package interfaces

---
+++ src/interfaces/school/Student.java
@@ -0,0 +1,31 @@
+package interfaces.school;
+
+public final class Student extends BasePerson {
+
+    private int grade;
+
+    Student(String firstName, String lastName,
+            char gender, int age, String status, int grade) {
+        super(firstName, lastName, gender, age, status);
+        this.grade = grade;
+    }
+
+    public final int getGrade() {
+        return grade;
+    }
+
+    public void printParent(Parent[] parents) {
+        for(Parent parent: parents) {
+            if (parent.getStudent().equals(this)) {
+                System.out.println();
+                System.out.println(
+                        "school.Student: " + getFirstName() + " " + getLastName() + ", grade " + getGrade()
+                );
+                System.out.println(
+                        "school.Parent: " + parent.getFirstName() + " " + parent.getLastName() +
+                                " phone number: " + parent.getPhoneNumber()
+                );
+            }
+        }
+    }
+}
